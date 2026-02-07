62c7f970bccecfc2a6a6c4fc780e7e513d655b18
Neo131088 <neo131088@gmail.com>
2026-02-07 13:29:10 +0100

    add

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 dotenv.config();
-import mongoose from 'mongoose';
+
 
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
@@ -12,19 +12,17 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRouter from './routes/notesRoutes.js';
-import 'dotenv/config'; // або dotenv.config();
 
-console.log('MONGODB_URL:', process.env.MONGODB_URL);
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('❌ Failed to connect to MongoDB:', err));
+
+await connectMongoDB();
+ 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
-app.use(express.json({ limit: '5mb' }));
-app.use(cors({ methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'] }));
-app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
 
 app.use(notesRouter);
 
@@ -34,7 +32,6 @@ app.use(errors());
 
 app.use(errorHandler);
 
-await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
