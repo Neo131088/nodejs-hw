abb85ae62191539dca6734eafca4d017f929f33e
Neo131088 <neo131088@gmail.com>
2026-02-09 18:48:49 +0100

    Fix validation and MongoDB connection

--- src/server.js
+++ src/server.js
@@ -1,37 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
-dotenv.config();
-
-
+import 'dotenv/config';
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
-import notesRouter from './routes/notesRoutes.js';
-
-
-await connectMongoDB();
- 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-
-app.use(notesRouter);
+app.use(authRoutes);
+app.use(notesRoutes);
 
 app.use(notFoundHandler);
-
 app.use(errors());
-
 app.use(errorHandler);
 
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
